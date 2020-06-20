422e51ca8b579694810eace97f4cebc56f4c49d1
Jared <1142367007@qq.com>
2020-06-20 21:34:41 +0800

    20200620修改

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,2 +1,42 @@
-aaaa;
-aaaa;
+$(function () {
+  $(".tui-search input").on("focus", () => {
+    $("#dropdown-menu").show();
+    $.getJSON({
+      url: "./data/hotsearch.json",
+      error: (error) => {
+        console.log(error);
+      },
+      success: (data) => {
+        console.log(data);
+        // let html = ``;
+        // let obj = JSON.parse(data, index);
+        // obj.data.data.foreach((val) => {
+        //   console.log(1);
+        //   html += `<li class="search-new">
+        //     <div>
+        //       <i class="tag-num num${index}"></i>
+        //       ${val}
+        //     </div>
+        //   </li>`;
+        // });
+        // $(".search-assn ul").append(html);
+      },
+    });
+  });
+  $.getJSON("../data/hotsearch.json", (data) => {
+    let obj = data;
+    let html = ``;
+    obj["data"]["data"].forEach((val, index) => {
+      html += `<li class="search-new">
+        <div>
+          <i class="tag-num num${index + 1}"></i>
+          ${val}
+        </div>
+      </li>`;
+    });
+    $(".search-assn ul").append(html);
+  });
+  $(".tui-search-input input").blur(() => {
+    $("#dropdown-menu").hide();
+  });
+});
--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,66 @@ const sass = require("gulp-sass");
 const uglify = require("gulp-uglify");
 const concat = require("gulp-concat");
 const rename = require("gulp-rename");
+const connect = require("gulp-connect");
 
 gulp.task("copyhtml", (done) => {
-  gulp.src("./html/*.html").pipe(gulp.dest("./dist/html"));
+  gulp
+    .src("./html/*.html")
+    .pipe(gulp.dest("./dist/html"))
+    .pipe(connect.reload());
   done();
 });
 gulp.task("copycss", (done) => {
-  gulp.src("./css/*.css").pipe(gulp.dest("./dist/css"));
+  gulp.src("./css/*.css").pipe(gulp.dest("./dist/css")).pipe(connect.reload());
   done();
 });
 gulp.task("copyjs", (done) => {
-  gulp.src("./js/*.js").pipe(gulp.dest("./dist/js"));
+  gulp.src("./js/*.js").pipe(gulp.dest("./dist/js")).pipe(connect.reload());
+  done();
+});
+gulp.task("copyimg", (done) => {
+  gulp.src("./img/**").pipe(gulp.dest("./dist/img")).pipe(connect.reload());
+  done();
+});
+gulp.task("copydata", (done) => {
+  gulp.src("./data/**").pipe(gulp.dest("./dist/data")).pipe(connect.reload());
+  done();
+});
+gulp.task("copyicon", (done) => {
+  gulp
+    .src("./iconfont/**")
+    .pipe(gulp.dest("./dist/iconfont"))
+    .pipe(connect.reload());
   done();
 });
 
 gulp.task("watch", (done) => {
   gulp.watch(
-    ["./html/*.html", "./css/*.css", "./js/*.js"],
-    gulp.series("copyhtml", "copycss", "copyjs")
+    [
+      "./html/*.html",
+      "./css/*.css",
+      "./js/*.js",
+      "./img/**",
+      "./data/**",
+      "./iconfont/**",
+    ],
+    gulp.series(
+      "copyhtml",
+      "copycss",
+      "copyjs",
+      "copyimg",
+      "copydata",
+      "copyicon"
+    )
   );
   done();
 });
-
-gulp.task("default", gulp.series("watch"));
+gulp.task("server", (done) => {
+  connect.server({
+    root: "dist",
+    livereload: true,
+    port: 5000,
+  });
+  done();
+});
+gulp.task("default", gulp.series("watch", "server"));
